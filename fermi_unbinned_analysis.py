b7be889a846b98e3d02ad31efa5cd0abf7c40222
ritamsaha00 <69363775+ritamsaha00@users.noreply.github.com>
2022-10-08 20:24:27 +0530

    more automated option

--- fermi_unbinned_analysis.py
+++ fermi_unbinned_analysis.py
@@ -3,8 +3,11 @@ from make4FGLxml import *
 import pyLikelihood
 from UnbinnedAnalysis import *
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import json
+import time
+from playsound import playsound
 
 print("________________________________________")
 print("****************************************")
@@ -15,13 +18,16 @@ print("________________________________________\n")
 #The source name input should not contain spaces
 name = input("Enter the name of the source (without spaces): ")
 
-data = open(name + "_data.txt", "r")
+data = open(name + ".data", "r")
 print(data.read()) #Prints the info in the data.txt file
 
 #cat_id = input("Enter the FAVA catalogue id of the source: ")
 f = open('catalogue.json')
 catalogue = json.load(f)
 cat_id = catalogue[name]["Gamma_ID"]
+
+query = pd.read_csv(name + "_query.csv")
+query = np.array(query)
 
 """
 Filter the data files according to the following conditions:
@@ -34,11 +40,11 @@ gt.filter['evclass'] = 128                                                      
 gt.filter['evtype']  = 3                                                         #NOTE: Both front and back convertion
 gt.filter['ra']      = catalogue[name]["RA"]                                     #NOTE: RA coordinate in degrees
 gt.filter['dec']     = catalogue[name]["Dec"]                                    #NOTE: Dec coordinate in degrees
-gt.filter['rad']     = float(input("Enter the search radius of the source: "))   #TODO: Search radius around the source
-gt.filter['tmin']    = int(input("Enter the Start time in MET format: "))        #TODO: MET Start time in seconds
-gt.filter['tmax']    = int(input("Enter the End time in MET format: "))          #TODO: MET End time in seconds
-gt.filter['emin']    = float(input("Enter the lower limit of energy in MeV: "))  #TODO: Minimum energy search in MeV
-gt.filter['emax']    = float(input("Enter the upper limit of energy in MeV: "))  #TODO: Maximum energy search in MeV
+gt.filter['rad']     = query[0][1] #float(input("Enter the search radius of the source: "))   #TODO: Search radius around the source
+gt.filter['tmin']    = query[1][1] #int(input("Enter the Start time in MET format: "))        #TODO: MET Start time in seconds
+gt.filter['tmax']    = query[2][1] #int(input("Enter the End time in MET format: "))          #TODO: MET End time in seconds
+gt.filter['emin']    = query[3][1] #float(input("Enter the lower limit of energy in MeV: "))  #TODO: Minimum energy search in MeV
+gt.filter['emax']    = query[4][1] #float(input("Enter the upper limit of energy in MeV: "))  #TODO: Maximum energy search in MeV
 gt.filter['zmax']    = 90                                                        #Maximum zenith angle, recommended angle is 90 degrees
 gt.filter['infile']  = '@'+ name +'_events.list'                                 #REVIEW: make it so that for a single photon file it's "name_PH.fits"
 gt.filter['outfile'] = name + '_filtered.fits'                                   #the name of the output filtered file
@@ -58,20 +64,22 @@ print("______________________________________")
 print("\n")
 
 print("Running the selection protocol of the data....\n")
+t1 = time.time()
 
 gt.filter.run() #command to run the filter protocol
 
-print("Done.........!!!!!!!!!!")
+t2 = time.time()
+print("Done.........!!!!!!!!!!\t", t2-t1, "s")
 
 """
 Make a good-time interval using the spacecraft file data
 """
 
-gt.maketime['scfile']  = name +'_SC00.fits' #Enter the spacecraft file
-gt.maketime['filter']  = '(DATA_QUAL>0) && (LAT_CONFIG==1)'
-gt.maketime['roicut']  = 'no' #ROI-Based Zenith Angle Cut(roicut)
-gt.maketime['evfile']  = name + '_filtered.fits' #the event data file
-gt.maketime['outfile'] = name +'_filtered_gti.fits' #the name of output file
+gt.maketime['scfile']  = name +'_SC00.fits'                 #Enter the spacecraft file
+gt.maketime['filter']  = '(DATA_QUAL>0) && (LAT_CONFIG==1)' #NOTE: see the readme file for more info
+gt.maketime['roicut']  = 'no'                               #ROI-Based Zenith Angle Cut(roicut)
+gt.maketime['evfile']  = name + '_filtered.fits'            #the event data file
+gt.maketime['outfile'] = name +'_filtered_gti.fits'         #the name of output file
 
 print("\n")
 print("______________________________________")
@@ -84,10 +92,12 @@ print("______________________________________")
 print("\n")
 
 print("Running the maketime protocol forgood time interval....\n")
+t1 = time.time()
 
 gt.maketime.run() #command to run the time selection protocol
 
-print("Done.........!!!!!!!!!!")
+t2 = time.time()
+print("Done.........!!!!!!!!!!\t", t2-t1, "s")
 
 """
 Creating a livetime cube
@@ -112,10 +122,12 @@ print("______________________________________")
 print("\n")
 
 print("Running the expCube protocol to create livetime cube.....\n")
+t1 = time.time()
 
 gt.expCube.run()
 
-print("Done.........!!!!!!!!")
+t2 = time.time()
+print("Done.........!!!!!!!!\t", t2-t1, "s")
 
 """ 
 Creating an exposure map
@@ -144,16 +156,20 @@ print("______________________________________")
 print("\n")
 
 print("Running the expMap protocol for exposure map\n")
+t1 = time.time()
 
 gt.expMap.run()
 
-print("Done..........!!!!!!!!!")
+t2 = time.time()
+print("Done..........!!!!!!!!!\t", t2-t1, "s")
 
 #REVIEW: what this part of the code doing?
-print("Running......") 
+print("Running......")
+t1 = time.time()
 mymodel = srcList('gll_psc_v28.xml', name + '_filtered_gti.fits', name + '_model.xml')
 mymodel.makeModel('/home/ray/miniconda3/envs/fermi/share/fermitools/refdata/fermi/galdiffuse/gll_iem_v07.fits', 'gll_iem_v07', '/home/ray/miniconda3/envs/fermi/share/fermitools/refdata/fermi/galdiffuse/iso_P8R3_SOURCE_V3_v1.txt', 'iso_P8R3_SOURCE_V3_v1')
-print("Done..........!!!!!!!!")
+t2 = time.time()
+print("Done..........!!!!!!!!", t2-t1, "s")
 
 gt.diffResps['evfile'] = name + '_filtered_gti.fits'
 gt.diffResps['scfile'] = name + '_SC00.fits'
@@ -170,10 +186,12 @@ print("______________________________________")
 print("\n")
 
 print("Running the diffResps protocol\n")
+t1 = time.time()
 
 gt.diffResps.run()
 
-print("Done........!!!!!!!!!")
+t2 = time.time()
+print("Done........!!!!!!!!!\t", t2-t1, "s")
 
 obs = UnbinnedObs(name + '_filtered_gti.fits',name + '_SC00.fits',expMap=name + '_expmap.fits',
 expCube=name + '_ltcube.fits',irfs='P8R3_SOURCE_V3')
@@ -195,26 +213,41 @@ else:
 like.tol = float(input("Enter the preferred tolerance value: ")) #TODO: Enter the preffered tolerance value
 likeobj = pyLike.NewMinuit(like.logLike)
 print("Running the fit protocol........")
+t1 = time.time()
 print(like.fit(verbosity = 0, covar = True, optObject = likeobj)) #NOTE: This step is running the model fitting
+t2 = time.time()
+print("Done.......!!!!!!\t", t2-t1, "s")
 
-like.plot() #NOTE: Plot the data
+playsound('/home/ray/Downloads/soundfx.d_jingle1.mp3') #NOTE: Just a fun little ditty for my personal amusement
+
+#like.plot() #NOTE: Plot the data
 
 #NOTE: The section below plots the data using matplotlib
-# E = (like.energies[:-1] + like.energies[1:]) / 2
-# plt.figure(figsize=(5,5))
-# plt.ylim((0.4, 1e4))
-# plt.xlim((100, 500000))
-# sum_model = np.zeros_like(like._srcCnts(like.sourceNames()[0]))
-# for sourceName in like.sourceNames():
-#     sum_model = sum_model + like._srcCnts(sourceName)
-#     plt.loglog(E, like._srcCnts(sourceName), label=sourceName[1:])
-# plt.loglog(E,sum_model,label='Total Model')
-# plt.errorbar(E,like.nobs,yerr=np.sqrt(like.nobs), fmt='o',label='Counts')
+E = (like.energies[:-1] + like.energies[1:]) / 2
+plt.figure(figsize=(5,5))
+plt.ylim((0.4, 1e4))
+plt.xlim((100, 500000))
+sum_model = np.zeros_like(like._srcCnts(like.sourceNames()[0]))
+for sourceName in like.sourceNames():
+    sum_model = sum_model + like._srcCnts(sourceName)
+    plt.loglog(E, like._srcCnts(sourceName), label=sourceName[1:])
+plt.loglog(E,sum_model,label='Total Model')
+plt.errorbar(E,like.nobs,yerr=np.sqrt(like.nobs), fmt='o',label='Counts')
 # ax = plt.gca()
 # box = ax.get_position()
 # ax.set_position([box.x0, box.y0, box.width * 0.5, box.height])
-# plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-# plt.show()
+#plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+plt.show()
+
+resid = (like.nobs - sum_model)/sum_model
+resid_err = (np.sqrt(like.nobs)/sum_model)
+plt.figure(figsize=(9,9))
+plt.xscale('log')
+plt.errorbar(E,resid,yerr=resid_err,fmt='o')
+plt.axhline(0.0,ls=':')
+plt.show()
+
+
 
 if likeobj.getRetCode() == 0:
     print("The likelihood analysis didn't converge\n")
