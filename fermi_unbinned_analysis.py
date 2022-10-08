3de3386929ad470e9f13d065d70e6440193fa7ca
ritamsaha00 <69363775+ritamsaha00@users.noreply.github.com>
2022-10-08 10:45:19 +0530

    more automated options

--- fermi_unbinned_analysis.py
+++ fermi_unbinned_analysis.py
@@ -3,6 +3,8 @@ from make4FGLxml import *
 import pyLikelihood
 from UnbinnedAnalysis import *
 import numpy as np
+import matplotlib.pyplot as plt
+import json
 
 print("________________________________________")
 print("****************************************")
@@ -10,21 +12,16 @@ print("This program is strictly for unbinned analysis")
 print("****************************************")
 print("________________________________________\n")
 
-
 #The source name input should not contain spaces
 name = input("Enter the name of the source (without spaces): ")
 
 data = open(name + "_data.txt", "r")
-print(data.read())
+print(data.read()) #Prints the info in the data.txt file
 
-#A way to automatically input the FAVA catalogue ID
-catalogue = {'NGC125' : '3FGL J0319.8+4130',
-    '4C+21.35': '3FGL J1224.9+2122',
-    '3C279' : '3FGL J1256.1-0547'
-}
-#cat_id = catalogue[name]
-
-cat_id = input("Enter the FAVA catalogue id of the source: ")
+#cat_id = input("Enter the FAVA catalogue id of the source: ")
+f = open('catalogue.json')
+catalogue = json.load(f)
+cat_id = catalogue[name]["Gamma_ID"]
 
 """
 Filter the data files according to the following conditions:
@@ -33,29 +30,29 @@ print("____________________________")
 print("Enter the following field")
 print("____________________________\n")
 
-gt.filter['evclass'] = 128 #*This event class has a residual background rate https://fermi.gsfc.nasa.gov/ssc/data/analysis/documentation/Cicerone/Cicerone_Data/LAT_DP.html
-gt.filter['evtype'] = 3 #*Both front and back convertion
-gt.filter['ra'] = float(input("Enter the RA value of source podition: ")) #TODO: RA coordinate in degrees
-gt.filter['dec'] = float(input("Enter the Dec value of source position: ")) #TODO: Dec coordinate in degrees
-gt.filter['rad'] = float(input("Enter the search radius of the source: ")) #TODO: Search radius around the source
-gt.filter['tmin'] = int(input("Enter the Start time in MET format: ")) #TODO: MET Start time in seconds
-gt.filter['tmax'] = int(input("Enter the End time in MET format: ")) #TODO: MET End time in seconds
-gt.filter['emin'] = float(input("Enter the lower limit of energy in MeV: ")) #TODO: Minimum energy search in MeV
-gt.filter['emax'] = float(input("Enter the upper limit of energy in MeV: ")) #TODO: Maximum energy search in MeV
-gt.filter['zmax'] = 90 #Maximum zenith angle, recommended angle is 90 degrees
-gt.filter['infile'] = '@'+ name +'_events.list' #the input file
-gt.filter['outfile'] = name + '_filtered.fits' #the name of the output filtered file
+gt.filter['evclass'] = 128                                                       #NOTE: This event class has a residual background rate
+gt.filter['evtype']  = 3                                                         #NOTE: Both front and back convertion
+gt.filter['ra']      = catalogue[name]["RA"]                                     #NOTE: RA coordinate in degrees
+gt.filter['dec']     = catalogue[name]["Dec"]                                    #NOTE: Dec coordinate in degrees
+gt.filter['rad']     = float(input("Enter the search radius of the source: "))   #TODO: Search radius around the source
+gt.filter['tmin']    = int(input("Enter the Start time in MET format: "))        #TODO: MET Start time in seconds
+gt.filter['tmax']    = int(input("Enter the End time in MET format: "))          #TODO: MET End time in seconds
+gt.filter['emin']    = float(input("Enter the lower limit of energy in MeV: "))  #TODO: Minimum energy search in MeV
+gt.filter['emax']    = float(input("Enter the upper limit of energy in MeV: "))  #TODO: Maximum energy search in MeV
+gt.filter['zmax']    = 90                                                        #Maximum zenith angle, recommended angle is 90 degrees
+gt.filter['infile']  = '@'+ name +'_events.list'                                 #REVIEW: make it so that for a single photon file it's "name_PH.fits"
+gt.filter['outfile'] = name + '_filtered.fits'                                   #the name of the output filtered file
 
 print("\n")
 print("______________________________________")
-print("Event class:\t 128 (residual background model)")
-print("Event type:\t 3 (both front and back conversion)")
-print("Position:\t (", gt.filter['ra'], ", ", gt.filter['dec'], ")")
-print("Search radius:\t", gt.filter['rad'])
-print("Energy:\t", gt.filter['emin'], ", ", gt.filter['emax'])
-print("Time [MET]:\t", gt.filter['tmin'], ", ", gt.filter['tmax'])
+print("Event class:         \t 128 (residual background model)")
+print("Event type:          \t 3 (both front and back conversion)")
+print("Position:            \t (", gt.filter['ra'], ", ", gt.filter['dec'], ")")
+print("Search radius:       \t", gt.filter['rad'])
+print("Energy:              \t", gt.filter['emin'], ", ", gt.filter['emax'])
+print("Time [MET]:          \t", gt.filter['tmin'], ", ", gt.filter['tmax'])
 print("Maximum Zenith Angle:\t", gt.filter['zmax'])
-print("The input file name:\t", gt.filter['infile'])
+print("The input file name: \t", gt.filter['infile'])
 print("The output file name:\t", gt.filter['outfile'])
 print("______________________________________")
 print("\n")
@@ -70,19 +67,19 @@ print("Done.........!!!!!!!!!!")
 Make a good-time interval using the spacecraft file data
 """
 
-gt.maketime['scfile'] = name +'_SC00.fits' #Enter the spacecraft file
-gt.maketime['filter'] = '(DATA_QUAL>0) && (LAT_CONFIG==1)'
-gt.maketime['roicut'] = 'no' #ROI-Based Zenith Angle Cut(roicut)
-gt.maketime['evfile'] = name + '_filtered.fits' #the event data file
+gt.maketime['scfile']  = name +'_SC00.fits' #Enter the spacecraft file
+gt.maketime['filter']  = '(DATA_QUAL>0) && (LAT_CONFIG==1)'
+gt.maketime['roicut']  = 'no' #ROI-Based Zenith Angle Cut(roicut)
+gt.maketime['evfile']  = name + '_filtered.fits' #the event data file
 gt.maketime['outfile'] = name +'_filtered_gti.fits' #the name of output file
 
 print("\n")
 print("______________________________________")
-print("Spacecrapt file:\t", gt.maketime['scfile'])
-print("Filter:\t", gt.maketime['filter'])
+print("Spacecrapt file:           \t", gt.maketime['scfile'])
+print("Filter:                    \t", gt.maketime['filter'])
 print("ROI-Based Zenith Angle Cut:\t", gt.maketime['roicut'])
-print("Event file:\t", gt.maketime['evfile'])
-print("Output file:\t", gt.maketime['outfile'])
+print("Event file:                \t", gt.maketime['evfile'])
+print("Output file:               \t", gt.maketime['outfile'])
 print("______________________________________")
 print("\n")
 
@@ -96,21 +93,21 @@ print("Done.........!!!!!!!!!!")
 Creating a livetime cube
 """
 
-gt.expCube['evfile'] = name + '_filtered_gti.fits' #the event data file
-gt.expCube['scfile'] = name + '_SC00.fits' #the spacecraft file
-gt.expCube['outfile'] = name + '_ltcube.fits' #the name of the output file
-gt.expCube['zmax'] = 90 #the maximum zenith angle
-gt.expCube['dcostheta'] = float(input("Enter the step size in cos(θ): ")) #TODO:step size in cos(\theta)
-gt.expCube['binsz'] = int(input("Enter the bin-size: ")) #TODO:Size of bins
+gt.expCube['evfile']    = name + '_filtered_gti.fits'                     #the event data file
+gt.expCube['scfile']    = name + '_SC00.fits'                             #the spacecraft file
+gt.expCube['outfile']   = name + '_ltcube.fits'                           #the name of the output file
+gt.expCube['zmax']      = 90                                              #the maximum zenith angle
+gt.expCube['dcostheta'] = float(input("Enter the step size in cos(θ): ")) #TODO: step size in cos(\theta)
+gt.expCube['binsz']     = int(input("Enter the bin-size: "))              #TODO: Size of bins
 
 print("\n")
 print("______________________________________")
-print("Event file:\t", gt.expCube['evfile'])
-print("Spacecraft file:\t", gt.expCube['scfile'])
-print("Output file:\t", gt.expCube['outfile'])
+print("Event file:          \t", gt.expCube['evfile'])
+print("Spacecraft file:     \t", gt.expCube['scfile'])
+print("Output file:         \t", gt.expCube['outfile'])
 print("Maximum zenith angle:\t", gt.expCube['zmax'])
-print("Step size in cos(θ):\t", gt.expCube['dcostheta'])
-print("Bin size:\t", gt.expCube['binsz'])
+print("Step size in cos(θ): \t", gt.expCube['dcostheta'])
+print("Bin size:            \t", gt.expCube['binsz'])
 print("______________________________________")
 print("\n")
 
@@ -124,25 +121,25 @@ print("Done.........!!!!!!!!")
 Creating an exposure map
 """
 
-gt.expMap['evfile'] = name + '_filtered_gti.fits'
-gt.expMap['scfile'] = name + '_SC00.fits'
-gt.expMap['expcube'] = name + '_ltcube.fits'
-gt.expMap['outfile'] = name + '_expmap.fits'
-gt.expMap['irfs'] = 'P8R3_SOURCE_V3' #input("Enter the response function [recommended: CALDB/P8R3_SOURCE_V3]: ")
-gt.expMap['srcrad'] = gt.filter['rad'] + 10 #Choose (search-radius + 10)
-gt.expMap['nlong'] = 120
-gt.expMap['nlat'] = 120
-gt.expMap['nenergies'] = int(input("Enter the energy bins[37]: "))
+gt.expMap['evfile']    = name + '_filtered_gti.fits' #the event data file
+gt.expMap['scfile']    = name + '_SC00.fits'         #the spacecraft file
+gt.expMap['expcube']   = name + '_ltcube.fits'       #the livetime-cube file
+gt.expMap['outfile']   = name + '_expmap.fits'       #name of the output file
+gt.expMap['irfs']      = 'P8R3_SOURCE_V3'            #input("Enter the response function [recommended: CALDB/P8R3_SOURCE_V3]: ")
+gt.expMap['srcrad']    = gt.filter['rad'] + 10       #Choose (search-radius + 10)
+gt.expMap['nlong']     = 120
+gt.expMap['nlat']      = 120
+gt.expMap['nenergies'] = int(input("Enter the energy bins[37]: ")) #TODO: Energy bin size
 
 print("\n")
 print("______________________________________")
-print("Event file:\t", gt.expMap['evfile'])
-print("Spacecraft file:\t", gt.expMap['scfile'])
+print("Event file:        \t", gt.expMap['evfile'])
+print("Spacecraft file:   \t", gt.expMap['scfile'])
 print("Livetime cube file:\t", gt.expMap['expcube'])
-print("Output file:\t", gt.expMap['outfile'])
-print("Response function:\t", gt.expMap['irfs'])
-print("Axis segments:\t", gt.expMap['nlong'], ", ", gt.expMap['nlat'])
-print("Energy bin:\t", gt.expMap['nenergies'])
+print("Output file:       \t", gt.expMap['outfile'])
+print("Response function: \t", gt.expMap['irfs'])
+print("Axis segments:     \t", gt.expMap['nlong'], ", ", gt.expMap['nlat'])
+print("Energy bin:        \t", gt.expMap['nenergies'])
 print("______________________________________")
 print("\n")
 
@@ -152,19 +149,22 @@ gt.expMap.run()
 
 print("Done..........!!!!!!!!!")
 
+#REVIEW: what this part of the code doing?
+print("Running......") 
 mymodel = srcList('gll_psc_v28.xml', name + '_filtered_gti.fits', name + '_model.xml')
-mymodel.makeModel('gll_iem_v07.fits', 'gll_iem_v07', 'iso_P8R3_SOURCE_V3_v1.txt', 'iso_P8R3_SOURCE_V3_v1')
+mymodel.makeModel('/home/ray/miniconda3/envs/fermi/share/fermitools/refdata/fermi/galdiffuse/gll_iem_v07.fits', 'gll_iem_v07', '/home/ray/miniconda3/envs/fermi/share/fermitools/refdata/fermi/galdiffuse/iso_P8R3_SOURCE_V3_v1.txt', 'iso_P8R3_SOURCE_V3_v1')
+print("Done..........!!!!!!!!")
 
 gt.diffResps['evfile'] = name + '_filtered_gti.fits'
 gt.diffResps['scfile'] = name + '_SC00.fits'
 gt.diffResps['srcmdl'] = name + '_model.xml'
-gt.diffResps['irfs'] = 'P8R3_SOURCE_V3'
+gt.diffResps['irfs']   = 'P8R3_SOURCE_V3'
 
 print("\n")
 print("______________________________________")
-print("Event file:\t", gt.diffResps['evfile'])
-print("Spacecraft file:\t", gt.diffResps['scfile'])
-print("Source model:\t", gt.diffResps['srcmdl'])
+print("Event file:       \t", gt.diffResps['evfile'])
+print("Spacecraft file:  \t", gt.diffResps['scfile'])
+print("Source model:     \t", gt.diffResps['srcmdl'])
 print("Response function:\t", gt.diffResps['irfs'])
 print("______________________________________")
 print("\n")
@@ -179,10 +179,11 @@ obs = UnbinnedObs(name + '_filtered_gti.fits',name + '_SC00.fits',expMap=name + 
 expCube=name + '_ltcube.fits',irfs='P8R3_SOURCE_V3')
 like = UnbinnedAnalysis(obs, name + '_model.xml',optimizer='NewMinuit')
 
-print("The obs object:")
+print("\nThe obs object:")
 print(obs)
-print("The like object:")
+print("\nThe like object:")
 print(like)
+print("\n")
 
 print(like.tol)
 
@@ -191,13 +192,29 @@ if like.tolType == 1:
 else:
     print("Tolerance type: Relative\n")
 
-like.tol = float(input("Enter the preferred tolerance value: "))
+like.tol = float(input("Enter the preferred tolerance value: ")) #TODO: Enter the preffered tolerance value
 likeobj = pyLike.NewMinuit(like.logLike)
 print("Running the fit protocol........")
-like.fit(verbosity = 0, covar = True, optObject = likeobj)
-print("The -log(likelihood) value")
-like.fit
-like.plot()
+print(like.fit(verbosity = 0, covar = True, optObject = likeobj)) #NOTE: This step is running the model fitting
+
+like.plot() #NOTE: Plot the data
+
+#NOTE: The section below plots the data using matplotlib
+# E = (like.energies[:-1] + like.energies[1:]) / 2
+# plt.figure(figsize=(5,5))
+# plt.ylim((0.4, 1e4))
+# plt.xlim((100, 500000))
+# sum_model = np.zeros_like(like._srcCnts(like.sourceNames()[0]))
+# for sourceName in like.sourceNames():
+#     sum_model = sum_model + like._srcCnts(sourceName)
+#     plt.loglog(E, like._srcCnts(sourceName), label=sourceName[1:])
+# plt.loglog(E,sum_model,label='Total Model')
+# plt.errorbar(E,like.nobs,yerr=np.sqrt(like.nobs), fmt='o',label='Counts')
+# ax = plt.gca()
+# box = ax.get_position()
+# ax.set_position([box.x0, box.y0, box.width * 0.5, box.height])
+# plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+# plt.show()
 
 if likeobj.getRetCode() == 0:
     print("The likelihood analysis didn't converge\n")
@@ -216,6 +233,6 @@ print(like.Ts(cat_id))
 print("The standard deviation ( TS value is ~ σ^2): ")
 print(np.sqrt(like.Ts(cat_id)))
 
-print("Writing the model fit results in an XML file")
+print("Writing the model fit results in an XML file......")
 like.logLike.writeXml(name + '_fit.xml')
 print("Done........!!!!!")
